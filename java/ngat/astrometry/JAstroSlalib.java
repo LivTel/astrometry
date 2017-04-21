73e7356deafdea277d3c51e3b4656398eca30346
Chris Mottram <C.Mottram@ljmu.ac.uk>
2017-04-21 15:56:39 +0100

    UT1UTC from +0.5 to +0.4

--- java/ngat/astrometry/JAstroSlalib.java
+++ java/ngat/astrometry/JAstroSlalib.java
@@ -55,12 +55,13 @@ public class JAstroSlalib implements AstroLib {
      *   = +0.0 seconds beginning 31 Jan 2016 at 0000 UTC      
      *   = -0.1 seconds beginning 24 Mar 2016 at 0000 UTC    
      *   = -0.2 seconds beginning 19 May 2016 at 0000 UTC    
-     *   =  -0.3 seconds beginning 01 Sep 2016 at 0000 UTC         
-     *   =  -0.4 seconds beginning 17 Nov 2016 at 0000 UTC                 
-     *   =  +0.6 seconds beginning 01 Jan 2017 at 0000 UTC
-     *   =  +0.5 seconds beginning 26 Jan 2017 at 0000 UTC
+     *   = -0.3 seconds beginning 01 Sep 2016 at 0000 UTC         
+     *   = -0.4 seconds beginning 17 Nov 2016 at 0000 UTC                 
+     *   = +0.6 seconds beginning 01 Jan 2017 at 0000 UTC
+     *   = +0.5 seconds beginning 26 Jan 2017 at 0000 UTC
+     *   = +0.4 seconds beginning 30 Mar 2017 at 0000 UTC
      */
-    static final double UT1UTC = +0.5;
+    static final double UT1UTC = +0.4;
 
     /**
      * Calls slaCldj with the specified parameters.
